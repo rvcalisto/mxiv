51b69a309f88f7c065dbd8ff82f617f3e16ee73d
rvcalisto <16952101+rvcalisto@users.noreply.github.com>
2024-06-24 02:37:51 -0300

    minor changes in properties and methods

--- src/components/statusBar.js
+++ src/components/statusBar.js
@@ -12,41 +12,54 @@
  */
 export const StatusBar = new class {
 
-  /** 
-   * Either status bar is visible.
-   * @type {Boolean}
-   */
-  visibility = true;
-
-  /**
-   * Old visibility value before fullscreen.
-   */
-  #oldVisibility = this.visibility;
-
   /**
    * Application name.
    */
   #titleSuffix = 'MXIV';
 
+  /**
+   * @type {HTMLDivElement}
+   */
+  #barElement = document.getElementById('bar');
+
+  /**
+   * @type {HTMLLabelElement}
+   */
+  #nameElement = document.getElementById('barName');
+
+  /**
+   * @type {HTMLLabelElement}
+   */
+  #infoElement = document.getElementById('barInfo');
+
+  /**
+   * Old visibility value before fullscreen.
+   */
+  #wasVisible = this.isVisible();
+
 
   // toggle visibility on fullscreen
   static {
     elecAPI.onFullscreen( function onFullscreenChange(e, isFullscreen) {
-      if (isFullscreen) this.#oldVisibility = visibility;
-      toggle(isFullscreen ? false : this.#oldVisibility);
-    })
+      if (isFullscreen) StatusBar.#wasVisible = StatusBar.isVisible();
+      StatusBar.toggle(isFullscreen ? false : StatusBar.#wasVisible);
+    });
+  }
+
+  /** 
+   * Either status bar is visible.
+   */
+  isVisible() {
+    return this.#barElement.style.fontSize === '';
   }
 
   /**
    * Toggle status bar visibility.
-   * @param {Boolean|undefined} force Either to force on and off instead.
+   * @param {Boolean} [force] Either to force on and off instead.
    */
   toggle(force) {
-    if (force === undefined) force = !visibility;
-    visibility = force;
-
-    const barElement = document.getElementById('bar');
-    barElement.style.fontSize = force ? '' : '0px'; // smooth animation
+    if (force === undefined) force = !this.isVisible();
+    this.#barElement.style.fontSize = force ? '' : '0px'; // smooth animation
   }
 
   /**
@@ -58,12 +71,9 @@ export const StatusBar = new class {
 
     document.title = title ? `${title} — ${this.#titleSuffix}` : this.#titleSuffix;
 
-    const nameElement = document.getElementById('barName');
-    nameElement.title = infoLeft;
-    nameElement.textContent = infoLeft;
-
-    const infoElement = document.getElementById('barInfo');
-    infoElement.textContent = infoRight;
-    nameElement.oncontextmenu = infoLeftFunc;
+    this.#nameElement.title = infoLeft;
+    this.#nameElement.textContent = infoLeft;
+    this.#nameElement.oncontextmenu = infoLeftFunc;
+    this.#infoElement.textContent = infoRight;
   }
 }
