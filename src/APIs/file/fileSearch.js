db9d0b6a1e79b20434e1d22ef5f995de6bf00afe
rvcalisto <16952101+rvcalisto@users.noreply.github.com>
2024-12-22 18:19:23 -0300

    clean up fileSearch.js for readability

--- src/APIs/file/fileSearch.js
+++ src/APIs/file/fileSearch.js
@@ -1,37 +1,69 @@
+// @ts-check
 import fs from 'fs';
 import p from 'path';
 import { expandPath, fileType } from './fileTools.js';
+
+
+/** 
+ * Wrapper for common file operations in unprivileged contexts.
+ * @typedef FileObject
+ * @property {string} path Absolute path to file.
+ * @property {string} name Basename. (Ex: duck.png)
+ * @property {'image'|'audio'|'video'|'folder'|'archive'} category File category.
+ */
+
+/**
+ * Object structure from folder's supported files.
+ * @typedef LSObject
+ * @property {FileObject} target Target folder object.
+ * @property {FileObject} upperDir Parent folder object.
+ * @property {FileObject[]} directories Listed directories.
+ * @property {FileObject[]} archives Supported archives.
+ * @property {FileObject[]} files Supported files.
+ */
 
 
 /**
  * LSObject cache for previously listed paths.
  * @type {Object<string, {lastModified:Number, lsObject:LSObject}>}
  */
-let lsCache = {}
+let lsCache = {};
 
 /**
  * File basenames for previously listed directories.
  * @type {Object<string, {lastModified:Number, hints:string[]}>}
  */
-let hintCache = {}
+let hintCache = {};
 
 /**
  * Clear all cache objects.
  */
 export function clearCache() {
-  lsCache = {}
-  hintCache = {}
+  lsCache = {};
+  hintCache = {};
 }
 
 /**
- * Object structure from folder's supported files.
- * @typedef {Object} LSObject
- * @property {FileObject[]} directories Listed directories.
- * @property {FileObject[]} archives Listed archives.
- * @property {FileObject[]} files Listed supported files.
- * @property {FileObject} target Target folder fileObject.
- * @property {FileObject} upperDir Target parent folder fileObject.
+ * Wrap file properties in object for use in unprivileged contexts.
+ * @param {'image'|'audio'|'video'|'folder'|'archive'} category File category.
+ * @param {string} name Basename. (Ex: `duck.png`)
+ * @param {string} fullpath Absolute path. (Ex: `/home/user/Pictures/duck.png`)
+ * @returns {FileObject} Wrapped file.
  */
+export const fileObj = (category, name, fullpath) => ({
+  category: category,
+  name: name,
+  path: fullpath
+});
+
+/**
+ * Return file stats. Null if unreachable.
+ * @param {string} filepath Absolute file path.
+ * @return {Promise<fs.Stats?>}
+ */
+const getFileStats = (filepath) => new Promise(resolve => {
+  fs.stat( filepath, (_err, stats) => resolve(stats) );
+});
 
 /**
  * Asynchronously list directory folders, media contents.
@@ -39,56 +71,49 @@ export function clearCache() {
  * @returns {Promise<LSObject>} Listed files separated by category.
  */
 export async function listFiles(path) {
+  const absolutePath = expandPath(path);
+  const lastModified = ( await getFileStats(absolutePath) )?.mtimeMs;
 
-  path = expandPath(path)
-
-  /**
-   * Last modified timestamp. Can be null if disabled or broken symlink.
-   * @type {Number?}
-   */
-  const lastModified = await new Promise(resolve => {
-    fs.stat( path, (err, stat) => resolve(stat?.mtimeMs) )
-  })
-  // if previously cached, use it if last modified match
-  const cacheEntry = lsCache[path]
+  // try a valid cached entry, if any
+  const cacheEntry = lsCache[absolutePath];
   if (cacheEntry && cacheEntry.lastModified === lastModified)
-    return cacheEntry.lsObject
+    return cacheEntry.lsObject;
 
-  const upperDir = p.dirname(path)
+  const parentDirectory = p.dirname(absolutePath);
+
+  /** @type {LSObject} */
   const lsObj = {
+    target: fileObj('folder', p.basename(absolutePath), absolutePath),
+    upperDir: fileObj('folder', p.basename(parentDirectory), parentDirectory),
     directories: [],
     archives: [],
-    files: [],
-    target: fileObj('folder', p.basename(path), path),
-    upperDir: fileObj('folder', p.basename(upperDir), upperDir) // for backward navigation
-  }
+    files: []
+  };
 
-  /**
-   * Dirent array, empty on error.
-   * @type {fs.Dirent[]}
-   */
+  /** @type {fs.Dirent[]} */
   const files = await new Promise(resolve => {
-    fs.readdir( path, { withFileTypes: true }, (err, files) => resolve(files) )
-  }) || []
-  
-  // filter items for desired types
+    fs.readdir( absolutePath, { withFileTypes: true }, (_err, files) => resolve(files) );
+  }) || [];
+
+  // wrap files by category
   for (let file of files) {
-    const fullPath = p.join(path, file.name)
-    const category = fileType(fullPath)
-    
+    const filepath = p.join(absolutePath, file.name);
+    const category = fileType(filepath);
+
     if ( !file.isFile() ) // assume is a directory
-      lsObj.directories.push( fileObj('folder', file.name, fullPath) )
+      lsObj.directories.push( fileObj('folder', file.name, filepath) );
     else if (category === 'archive')
-      lsObj.archives.push( fileObj(category, file.name, fullPath) )
+      lsObj.archives.push( fileObj(category, file.name, filepath) );
     else if (category !== 'other')
-      lsObj.files.push( fileObj(category, file.name, fullPath) )
+      lsObj.files.push( fileObj(category, file.name, filepath) );
   }
 
-  // store/update cache for path
-  if (lastModified != null) lsCache[path] = {
-    lastModified: lastModified,
-    lsObject: lsObj
-  }
+  // store/update cache
+  if (lastModified != null)
+    lsCache[absolutePath] = {
+      lastModified: lastModified,
+      lsObject: lsObj
+    };
 
   return lsObj;
 }
@@ -96,84 +121,55 @@ export async function listFiles(path) {
 /**
  * Return matching file basenames for queried filepath. 
  * * Used on Viewer 'open' action to hint files in a given directory.
- * @param {String} queryPath Path to list files.
- * @returns {Promise<String[]>}
+ * @param {string} queryPath Path to list files.
+ * @returns {Promise<string[]>}
  */
 export async function listPaths(queryPath) {
-  // treat working path
-  let workingPath = expandPath( queryPath || process.cwd() )
+  let workingPath = expandPath( queryPath || process.cwd() );
+  const stats = await getFileStats(workingPath);
 
-  /**
-   * Path FS.Stats. Can be null.
-   * @type {fs.Stats?}
-   */
-  const stats = await new Promise(resolve => {
-    fs.stat( workingPath, (err, stat) => resolve(stat) )
-  })
-
-  // malformed path, use parent dir and cached lastModified timestamp. 
-  // If parent not in cache, fake lastModified to allow caching
-  let lastModified = stats?.mtimeMs
-  if (!stats) {
-    workingPath = p.dirname(workingPath)
-    lastModified = hintCache[workingPath]?.lastModified || 0
+  // invalid stats may signal a malformed path, use parent's path & last modified
+  // timestamp. If parent not in cache, fake last modified timestamp to allow caching.
+  let lastModified = stats?.mtimeMs;
+  if (stats == null) {
+    workingPath = p.dirname(workingPath);
+    lastModified = hintCache[workingPath]?.lastModified || 0;
   }
 
-  // try cache, otherwise build and store
-  let hints
-  const cacheEntry = hintCache[workingPath]
+  // try a valid cache entry, otherwise build and store
+  let hints;
+  const cacheEntry = hintCache[workingPath];
   if (cacheEntry && cacheEntry.lastModified === lastModified) {
-    hints = cacheEntry.hints
+    hints = cacheEntry.hints;
   } else {
-    const lsObj = await listFiles(workingPath)
-    
-    // concat in order, append separator to directories
-    const directories = lsObj.directories.map(i => i.name + p.sep)
-    const archives = lsObj.archives.map(i => i.name)
-    const files = lsObj.files.map(i => i.name)
-    hints = directories.concat(archives, files)
+    const lsObj = await listFiles(workingPath);
 
-    // cache
-    if (lastModified != null) hintCache[workingPath] = {
-      lastModified: lastModified,
-      hints: hints
-    }
-  }
-  
-  // filter hints skipping query mismatches and unrequested dot files
-  const query = stats ? '' : p.basename(queryPath).toLowerCase()
-  const hintRoot = stats ? queryPath : p.dirname(queryPath)
-  const queryIsDot = stats ? queryPath.endsWith('.') : query[0] === '.'
+    // map to basename, append separator to directories, concat in order
+    const directories = lsObj.directories.map(i => i.name + p.sep);
+    const archives = lsObj.archives.map(i => i.name);
+    const files = lsObj.files.map(i => i.name);
+    hints = directories.concat(archives, files);
 
-  let filteredHints = []
-  for (const file of hints) {
-    if ( query && !file.toLowerCase().includes(query) ) continue
-    if (file[0] === '.' && !queryIsDot) continue
-    filteredHints.push( p.join(hintRoot, file) )
+    if (lastModified != null)
+      hintCache[workingPath] = {
+        lastModified: lastModified,
+        hints: hints
+      };
   }
 
-  return filteredHints
-}
+  // filter hints treating stat mismatches and unrequested dot files
+  const query = stats ? '' : p.basename(queryPath).toLowerCase();
+  const hintRoot = stats ? queryPath : p.dirname(queryPath);
+  const queryIsDot = stats ? queryPath.endsWith('.') : query[0] === '.';
 
-/** 
- * Wrapper for common file operations in unprivileged contexts.
- * @typedef {Object} FileObject
- * @property {String} path Absolute path to file.
- * @property {String} name Basename. (Ex: duck.png)
- * @property {'image'|'audio'|'video'|'folder'|'archive'} category File category.
- */
+  const filteredHints = [];
+  for (const basename of hints) {
+    if (basename[0] === '.' && !queryIsDot)
+      continue;
 
-/**
- * Wrap file properties in object for use in unprivileged contexts.
- * @param {'image'|'audio'|'video'|'folder'|'archive'} category File category.
- * @param {String} name Basename. (Ex: `duck.png`)
- * @param {String} fullpath Absolute path. (Ex: `/home/user/Pictures/duck.png`)
- * @returns {FileObject} Wrapped file.
- */
-export function fileObj(category, name, fullpath) {
-  return {
-    path : fullpath,
-    name : name,
-    category : category
+    if ( basename.toLowerCase().includes(query) )
+      filteredHints.push( p.join(hintRoot, basename) );
   }
+
+  return filteredHints;
 }
