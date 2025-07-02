2a464ee5c9cf625d94aefee4e62a6fb4f87b6684
rvcalisto <16952101+rvcalisto@users.noreply.github.com>
2025-07-02 18:15:30 -0300

    avoid tilde expansion for relative files starting with '~' in expandPath() tidy JSDoc annotations

--- src/APIs/file/fileTools.js
+++ src/APIs/file/fileTools.js
@@ -5,10 +5,10 @@ import { homedir } from 'os';
 
 
 /**
- * Infer type from supported file extentions.
+ * Infer type from supported file extensions.
  * For others such as folders, use async `fs.stat` instead.
- * @param {String} file File path or basename.
- * @returns Type String.
+ * @param {string} file File path or basename.
+ * @returns Type string.
  */
 export function fileType(file) {
 
@@ -17,41 +17,44 @@ export function fileType(file) {
     case '.gif': case '.apng': case '.webp':
     case '.svg': case '.icns': case '.ico':
     case '.avif':
-      return 'image'
+      return 'image';
     case '.mp3': case '.ogg': case '.wav':
     case '.m4a': case '.aac': case '.flac':
     case '.opus':
-      return 'audio'
+      return 'audio';
     case '.mp4': case '.webm': case '.mov':
       return 'video';
     case '.zip': case '.cbz':
-      return 'archive'
+      return 'archive';
   }
-  return 'other'
+
+  return 'other';
 }
 
 /**
  * Expand shortcuts and relative paths into absolute, atomic paths.
- * @param {String} path Path to expand.
- * @returns {String}
+ * @param {string} path Path to expand.
+ * @returns {string}
  */
 export function expandPath(path) {
-  if (path[0] === '~') path = homedir() + path.slice(1);
-  return p.resolve(path) // resolve "./", "../" & other relative paths
+  if ( path.startsWith(`~${p.sep}`) )
+    path = path.replace('~', homedir() );
+
+  return p.resolve(path); // resolve relative paths
 }
 
 /**
  * Delete file at given path. 
  * - Used in Viewer to delete current file at user order.
- * @param {String} filePath Absolute file path.
- * @returns {Boolean} Success.
+ * @param {string} filePath Absolute file path.
+ * @returns {boolean} Success.
  */
 export function deleteFile(filePath) {
   try {
-    rmSync(filePath)
-    return true
+    rmSync(filePath);
+    return true;
   } catch (err) {
-    return err.code === 'ENOENT' // already deleted, return true
+    return err.code === 'ENOENT'; // already deleted, return true
   }
 }
 
@@ -66,21 +69,21 @@ export function deleteFile(filePath) {
  * // as arguments instead of escaping quotes for complex scripts.
  * runOnFile("~/myScript.sh %F", "path/to/file.png")
  * 
- * @param {String} script User script.
+ * @param {string} script User script.
  * @param {string} [filepath] Absolute path to file to run script on.
- * @returns {Boolean} Success.
+ * @returns {boolean} Success.
  */
 export function runOnFile(script, filepath) {
-  const needFile = script.includes('%F') || script.includes('%N') || script.includes('%T')
-  if (needFile && !filepath) return false
+  const needFile = script.includes('%F') || script.includes('%N') || script.includes('%T');
+  if (needFile && !filepath)
+    return false;
 
   if (filepath) {
     script = script.replaceAll('%F', `"${filepath}"`)
-    script = script.replaceAll('%N', `"${p.basename(filepath)}"`)
-    script = script.replaceAll('%T', fileType(filepath) )
+                   .replaceAll('%N', `"${p.basename(filepath)}"`)
+                   .replaceAll('%T', fileType(filepath) );
   }
 
-  // console.log(`MXIV: Ran user script:\n${script}`)
-  exec(script) // non blocking
-  return true
+  exec(script); // non blocking
+  return true;
 }
