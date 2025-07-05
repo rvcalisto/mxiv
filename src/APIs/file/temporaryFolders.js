9681672960999280c737110cdedb493679abc16d
rvcalisto <16952101+rvcalisto@users.noreply.github.com>
2025-07-05 16:56:24 -0300

    better handle possible exceptions when handling archives reduce temporary folder logs to exceptions fix some JSDoc annotations

--- src/APIs/file/temporaryFolders.js
+++ src/APIs/file/temporaryFolders.js
@@ -4,6 +4,7 @@ import { tmpdir } from 'os';
 import { fileType } from './fileTools.js';
 import * as archiveTool from '../tool/archive.js';
 
+
 /**
  * Temporary directory where to extract archives.
  */
@@ -22,14 +23,14 @@ export const TemporaryFolders = new class {
 
   /**
    * Track archives temporarily extracted and their consumers.
-   * @type {Object<string, {path:String, owners:Set<string>}>}
+   * @type {Object<string, {path:string, owners:Set<string>}>}
    */
   #openArchives = {};
 
   /**
    * Return either path is from a temporary folder file.
-   * @param {String} path Path to file to verify.
-   * @returns {Boolean}
+   * @param {string} path Path to file to verify.
+   * @returns {boolean}
    */
   isPathFromTmp(path) {
     const pathIsTmp = path.includes( p.join(TMPDIR, TMPPREFIX) );
@@ -40,8 +41,8 @@ export const TemporaryFolders = new class {
 
   /**
    * Revoke access to a leased archive. Delete orphaned lease paths.
-   * @param {String} archivePath Archive to revoke access to.
-   * @param {String} ownerID Consumer UUID.
+   * @param {string} archivePath Archive to revoke access to.
+   * @param {string} ownerID Consumer UUID.
    */
   #revokeAccess(archivePath, ownerID) {
     const registry = this.#openArchives[archivePath];
@@ -64,14 +65,14 @@ export const TemporaryFolders = new class {
     return archivedFiles.some(filePath => {
       const type = fileType(filePath);
       return type === 'image' || type === 'video';
-    })
+    });
   }
 
   /**
    * Lease temporary folder for archive files. Returns folder path or empty if invalid.
-   * @param {String} archivePath Absolute path to archive.
-   * @param {String} ownerID Owner ID to register folder.
-   * @returns {Promise<String>} Path to temporary folder. Empty on failure.
+   * @param {string} archivePath Absolute path to archive.
+   * @param {string} ownerID Owner ID to register folder.
+   * @returns {Promise<string>} Path to temporary folder. Empty on failure.
    */
   async leaseArchive(archivePath, ownerID) {
 
@@ -84,10 +85,20 @@ export const TemporaryFolders = new class {
     if ( !await this.#archiveIsValid(archivePath) )
       return '';
 
-    // new unique temp folder (ex: /tmp/prefix-dpC7Id)
-    const tmpDir = fs.mkdtempSync( p.join(TMPDIR, TMPPREFIX) );
-    await archiveTool.extract(archivePath, tmpDir);
-    console.log(`MXIV: Created tmp folder at ${tmpDir}`);
+    // create unique temporary folder (ex: /tmp/prefix-dpC7Id)
+    let tmpDir = '';
+    try {
+      tmpDir = fs.mkdtempSync( p.join(TMPDIR, TMPPREFIX) );
+    } catch(err) {
+      console.log(`MXIV: Failed to created tmp folder at ${tmpDir}\n`, err);
+      return '';
+    }
+
+    const success = await archiveTool.extract(archivePath, tmpDir);
+    if (!success) {
+      console.log(`MXIV: Failed to extract ${archivePath} to ${tmpDir}`);
+      return '';
+    }
 
     this.#openArchives[archivePath] = {
       path: tmpDir,
@@ -100,8 +111,8 @@ export const TemporaryFolders = new class {
   /**
    * Revoke all archive accesses associated with ownerID. Spare archives if given.
    * Called after successfuly generating a new book for viewing or when closing the app.
-   * @param {String} ownerID Tab instance to be cleaned.
-   * @param {String[]} [spareArchives] Archive paths to spare.
+   * @param {string} ownerID Tab instance to be cleaned.
+   * @param {string[]} [spareArchives] Archive paths to spare.
    */
   surrenderLeases(ownerID, spareArchives = []) {
     for (const archive in this.#openArchives) {
@@ -112,7 +123,7 @@ export const TemporaryFolders = new class {
 
   /**
    * Delete temporary folder and its contents.
-   * @param {String} folder Temporary folder path to remove.
+   * @param {string} folder Temporary folder path to remove.
    */
   #deleteTmpFolder(folder) {
     // folder resides on TMPDIR, right? RIGHT?
@@ -124,7 +135,6 @@ export const TemporaryFolders = new class {
     // delete folder recursively
     try {
       fs.rmSync(folder, { recursive: true });
-      console.log(`MXIV: Deleted tmp folder at ${folder}`);
     } catch (err) {
       console.error(`MXIV: Failed to clear tmp folder at ${folder}\n`, err);
     }
